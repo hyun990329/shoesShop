c41037569340f109dd372f36cad3c247cd8c1a73
hyun990329 <dlrkdgus1999@gmail.com>
2025-09-16 14:52:02 +0900

    홈 디자인 진행

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
 import './App.css'
 import AppNavbar from './AppNavBar'
-
-
+// asset 폴더 내의 이미지 사용법 -> import 해서 사용
+import bg_png from './assets/images/bg.png'
+import {Container, Row, Col} from 'react-bootstrap';
+import img_2 from './assets/images/shoes2.jpg';
+import data from './data/data';
+import { useState } from 'react';
 
 
 function App() {
 
+  // 상품 정보를 갖는 product state 를 만든다
+  const [product, setProduct] = useState(data)
+
   return (
     <>
+      {/* 네비게이션 바 영역 시작 */}
       <AppNavbar />
+      {/* 네비게이션 바 영역 끝 */}
+
+      {/* 메인 대문 사진 영역 시작 */}
+      <div className='main-bg'
+        style={{backgroundImage: `url('${bg_png}')`}}
+      />
+      {/* 메인 대문 사진 영역 끝 */}
+
+      {/* 상품 진열 영역 시작 */}
+
+      {/* 상품 진열 영역 끝 */}
+         <Container>
+           <Row>
+            <Col className='text-center'>
+              <img src="https://zzzmini.github.io/images/shoes1.jpg" width='80%'/>
+              <h4>상품명</h4>
+              <p>상품 설명</p>
+            </Col>
+            <Col className='text-center'>
+              <img src={img_2} width='80%'/>
+              <h4>상품명</h4>
+              <p>상품 설명</p>
+            </Col>
+            <Col className='text-center'>
+              <img src="./images/shoes3.jpg" width='80%'/>
+              <h4>상품명</h4>
+              <p>상품 설명</p>
+            </Col>
+          </Row>
+         </Container>
     </>
   )
 }

347e2a3f5ec1987ad2004f6969b526b790ae9f5b
hyun990329 <dlrkdgus1999@gmail.com>
2025-09-16 17:28:32 +0900

    About 기능까지 완

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import {Container, Row, Col} from 'react-bootstrap';
 import data from "./data/data";
 import { useState } from "react";
 import Product from "./Product";
+import {Routes, Route} from "react-router-dom"
+import Detail from "./Detail";
+import About from "./About";
 
 function App() {
   // 상품정보를 갖는 product 스테이트를 만든다.
@@ -16,15 +19,26 @@ function App() {
       {/* 네비게이션 바 영역 시작 */}
       <AppNavBar />
       {/* 네비게이션 바 영역 끝 */}
-
-      {/* 메인 대문사진 영역 시작 */}
+      
+        {/* 메인 대문사진 영역 시작 */}
       <div className="main-bg" 
         style={{backgroundImage: `url('${bg_png}')`}}  
       />
       {/* 메인 대문사진 영역 끝 */}
+
+      {/* Routing 정보를 모아두는 영역 */}
+      {/* Spring 에서 사용하는 컨트롤러 역할 */}
+        <Routes>
+          <Route path="/" element={<div>메인페이지</div>} />
+          <Route path="/detail" element={<div><Detail /></div>} />
+          <Route path="/cart" element={<div>장바구니페이지</div>} />
+          <Route path="/about" element={<div><About /></div>} />
+          <Route path="*" element={<div>Page Not Found</div>}></Route>
+        </Routes>
+
+    
+
       {/* 상품진열영역 시작 */}
-      
-      {/* 상품진열영역 끝 */}
       <Container>
         <Row>
           {
@@ -39,6 +53,8 @@ function App() {
           }
         </Row>
       </Container>
+      {/* 상품진열영역 끝 */}
+      
     </>
   );
 }
--- src/AppNavBar/index.jsx
+++ src/AppNavBar/index.jsx
@@ -4,9 +4,13 @@ import './AppNavBar.css'
 // Route : 컴포넌트 이동
 // Routes : URL 에 담겨있는 정보를 획득
 // Link : 실제로 페이지를 이동, Link 위치의 컴포넌트를 뿌려줌
-import {Route, Routes, Link} from "react-router-dom"
+import {Link, useNavigate} from "react-router-dom"
+
 
 function AppNavbar() {
+
+  const navigate = useNavigate();
+
   return(
     <>
     <div className='App'>
@@ -14,19 +18,14 @@ function AppNavbar() {
           <Container>
             <Navbar.Brand href="#home">MuJinJang</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link><Link to={"/"}>Home</Link></Nav.Link>
-              <Nav.Link><Link to={"/detail"}>Detail</Link></Nav.Link>
-              <Nav.Link><Link to={"/cart"}>Cart</Link></Nav.Link>
+              <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
+              <Nav.Link onClick={()=>{navigate('detail')}}>Detail</Nav.Link>
+              <Nav.Link onClick={()=>{navigate('cart')}}>Cart</Nav.Link>
+              <Nav.Link onClick={()=>{navigate('about')}}>About</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
-        {/* Routing 정보를 모아두는 영역 */}
-        {/* Spring 에서 사용하는 컨트롤러 역할 */}
-        <Routes>
-          <Route path="/" element={<div>메인페이지</div>} />
-          <Route path="/detail" element={<div>상세페이지</div>} />
-          <Route path="/cart" element={<div>장바구니페이지</div>} />
-        </Routes>
+        
       </div>
     </>
   )

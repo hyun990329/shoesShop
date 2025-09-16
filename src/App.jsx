eb438a06562255518cc1db2e0f2a63f74dd50387
hyun990329 <dlrkdgus1999@gmail.com>
2025-09-16 15:52:51 +0900

    상품 완

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,47 @@
-import './App.css'
-import AppNavbar from './AppNavBar'
-// asset 폴더 내의 이미지 사용법 -> import 해서 사용
-import bg_png from './assets/images/bg.png'
+import "./App.css";
+import AppNavBar from "./AppNavBar";
+// assets 폴더 내의 이미지 사용법 -> import 해서 사용
+import bg_png from "./assets/images/bg.png"
+import img_2 from "./assets/images/shoes2.jpg"
 import {Container, Row, Col} from 'react-bootstrap';
-import img_2 from './assets/images/shoes2.jpg';
-import data from './data/data';
-import { useState } from 'react';
-
+import data from "./data/data";
+import { useState } from "react";
+import Product from "./Product";
 
 function App() {
-
-  // 상품 정보를 갖는 product state 를 만든다
-  const [product, setProduct] = useState(data)
+  // 상품정보를 갖는 product 스테이트를 만든다.
+  const [product, setProduct] = useState(data);
 
   return (
     <>
       {/* 네비게이션 바 영역 시작 */}
-      <AppNavbar />
+      <AppNavBar />
       {/* 네비게이션 바 영역 끝 */}
 
-      {/* 메인 대문 사진 영역 시작 */}
-      <div className='main-bg'
-        style={{backgroundImage: `url('${bg_png}')`}}
+      {/* 메인 대문사진 영역 시작 */}
+      <div className="main-bg" 
+        style={{backgroundImage: `url('${bg_png}')`}}  
       />
-      {/* 메인 대문 사진 영역 끝 */}
-
-      {/* 상품 진열 영역 시작 */}
-
-      {/* 상품 진열 영역 끝 */}
-         <Container>
-           <Row>
-            <Col className='text-center'>
-              <img src="https://zzzmini.github.io/images/shoes1.jpg" width='80%'/>
-              <h4>상품명</h4>
-              <p>상품 설명</p>
-            </Col>
-            <Col className='text-center'>
-              <img src={img_2} width='80%'/>
-              <h4>상품명</h4>
-              <p>상품 설명</p>
-            </Col>
-            <Col className='text-center'>
-              <img src="./images/shoes3.jpg" width='80%'/>
-              <h4>상품명</h4>
-              <p>상품 설명</p>
-            </Col>
-          </Row>
-         </Container>
+      {/* 메인 대문사진 영역 끝 */}
+      {/* 상품진열영역 시작 */}
+      
+      {/* 상품진열영역 끝 */}
+      <Container>
+        <Row>
+          {
+            product.map((shoes, _)=>{
+              return(
+                <Col key={shoes.id} className="text-center">
+                  {/* Product 콤포넌트 자리 */}
+                  <Product shoes={shoes}/>
+                </Col>
+              )
+            })
+          }
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/data/data.js
+++ src/data/data.js
@@ -1,21 +1,23 @@
-export default
-[
+export default [
   {
-    id : 0,
-    title : "White and Black",
-    content : "Born in France",
-    price : 120000
+    id: 0,
+    title: "White and Black",
+    content: "Born in France",
+    price: 120000,
+    imgSrc: './images/shoes1.jpg'
   },
   {
-    id : 1,
-    title : "Red Knit",
-    content : "Born in Seoul",
-    price : 110000
+    id: 1,
+    title: "Red Knit",
+    content: "Born in Seoul",
+    price: 110000,
+    imgSrc: './images/shoes2.jpg'
   },
   {
-    id : 2,
-    title : "Grey Yordan",
-    content : "Born in the States",
-    price : 130000
-  }
-]
+    id: 2,
+    title: "Grey Yordan",
+    content: "Born in the States",
+    price: 130000,
+    imgSrc: './images/shoes3.jpg'
+  },
+];

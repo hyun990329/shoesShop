db2d0733ad680daad6d3fa651848c52197a21e0f
hyun990329 <dlrkdgus1999@gmail.com>
2025-09-18 15:32:59 +0900

    useEffect 처리

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import "./App.css";
 import AppNavBar from "./AppNavBar";
 // assets 폴더 내의 이미지 사용법 -> import 해서 사용
 import bg_png from "./assets/images/bg.png"
-import {Container, Row, Col} from 'react-bootstrap';
 import data from "./data/data";
 import { useState } from "react";
-import Product from "./Product";
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import Detail from "./Detail";
 import About from "./About";
+import Home from "./Home";
+import House from "./House";
 
 function App() {
   // 상품정보를 갖는 product 스테이트를 만든다.
@@ -19,42 +19,27 @@ function App() {
       {/* 네비게이션 바 영역 시작 */}
       <AppNavBar />
       {/* 네비게이션 바 영역 끝 */}
-      
-        {/* 메인 대문사진 영역 시작 */}
+      {/* 메인 대문사진 영역 시작 */}
       <div className="main-bg" 
         style={{backgroundImage: `url('${bg_png}')`}}  
       />
       {/* 메인 대문사진 영역 끝 */}
-
-      {/* Routing 정보를 모아두는 영역 */}
-      {/* Spring 에서 사용하는 컨트롤러 역할 */}
-        <Routes>
-          <Route path="/" element={<div>메인페이지</div>} />
-          <Route path="/detail" element={<div><Detail /></div>} />
-          <Route path="/cart" element={<div>장바구니페이지</div>} />
-          <Route path="/about" element={<div><About /></div>} />
-          <Route path="*" element={<div>Page Not Found</div>}></Route>
-        </Routes>
-
-    
-
-      {/* 상품진열영역 시작 */}
-      <Container>
-        <Row>
-          {
-            product.map((shoes, _)=>{
-              return(
-                <Col key={shoes.id} className="text-center">
-                  {/* Product 콤포넌트 자리 */}
-                  <Product shoes={shoes}/>
-                </Col>
-              )
-            })
-          }
-        </Row>
-      </Container>
-      {/* 상품진열영역 끝 */}
-      
+      {/* Routing 정보를 한꺼번에 모아놓는 장소 */}
+      {/* 스프링에서 사용하는 컨트롤러 클래스 */}
+      <Routes>
+        <Route path="/" element={<Home product={product}/>} />
+        {/* /detail/2 -> PathVariable 설정 법 */}
+        <Route path="/detail/:id" element={<Detail product={product}/>} />
+        <Route path="/cart" element={<div>장바구니페이지</div>} />
+        <Route path="/house" element={<House />} />
+        {/* 중첩라우팅 처리 */}
+        <Route path="/about" element={<About/>}>
+          {/* /about/member */}
+          <Route path="member" element={<div>Member Page</div>}></Route>
+          <Route path="location" element={<div>Location Page</div>}></Route>
+        </Route>
+        <Route path="*" element={<div>Page Not Found 404 Error</div>}></Route>
+      </Routes>
     </>
   );
 }

a2d979fdee9d2a3023ecd81ee85adf540f344b1e
hyun990329 <dlrkdgus1999@gmail.com>
2025-09-19 10:15:08 +0900

    탭 구조 및 애니메이션 추가

--- src/TabContent/index.jsx
+++ src/TabContent/index.jsx
@@ -1,10 +1,30 @@
+import { use, useEffect, useState } from "react";
+
 function TabContent({tabState}){
+  // fade 값으로 css 를 지정하도록 함
+  let [fade, setFade] = useState('')
+
+
+  // useEffect 로 timer 를 실행
+  // 0.2초 후에 fade = ani_end 로 변경
+  useEffect(()=>{
+    let timer = setTimeout(()=>{
+      setFade('ani_end')
+    }, 100)
+    return(()=>{
+      clearTimeout(timer)
+      setFade('')
+    })
+  }, [tabState])
+
   return(
-    [
+    <div className={`ani_start ${fade}`}>
+    {[
       <div>내용 1</div>,
       <div>내용 2</div>,
       <div>내용 3</div>,
-    ][tabState]
+    ][tabState]}
+    </div>
   )
 }
 export default TabContent;
